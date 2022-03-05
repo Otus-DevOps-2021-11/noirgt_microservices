722f2da07c41443294ad0f1b4dc46b1f517dfc52
Геннадий Токарев <gtokarev@innoseti.ru>
2022-03-05 12:02:01 +0200

    Added new parameters

--- docker-monolith/infra/terraform/main.tf
+++ docker-monolith/infra/terraform/main.tf
@@ -18,32 +18,40 @@ provider "yandex" {
 }
 
 module "vpc" {
+  count                    = var.subnet_id == false ? 1 : 0
+
   source                   = "./modules/vpc"
   network_name             = "reddit-app-network"
-  subnet_name              = "reddit-docker-subnet"
-  v4_cidr_blocks           = ["192.168.8.0/24"]
+  subnet_name              = "${var.subnet_name}"
+  v4_cidr_blocks           = ["${var.subnet_cidr}"]
 }
 
-module "vm" {
-  for_each                 = {
+locals {
+  vms                      = {
     "gitlab-ci"    : {"cpu": 2, "memory": 6},
     "gitlab-runner": {"cpu": 2, "memory": 4},
     "app-docker"   : {"cpu": 2, "memory": 2},
     "registry-hub" : {"cpu": 2, "memory": 2}
+  }
+}
+
+module "vm" {
+  for_each                 = var.vm_name == false ? local.vms : {
+    "${var.vm_name}": {"cpu": "${var.vm_cpu}", "memory": "${var.vm_memory}"}
   }
 
   source                   = "./modules/vm"
   vm_name                  = "vm-${each.key}"
   public_key_path          = var.public_key_path
   vm_disk_image            = var.vm_disk_image
-  subnet_id                = module.vpc.vm-subnet.id
+  subnet_id                = var.subnet_id == false ? module.vpc[0].vm-subnet.id : var.subnet_id
   private_key_path         = var.private_key_path
 
   cpu                      = each.value["cpu"]
   memory                   = each.value["memory"]
 }
 
-module "ansiblecall" {
+/* module "ansiblecall" {
   source                   = "./modules/ansiblecall"
   private_key_path         = var.private_key_path
   playbook                 = "gitlab-ci.yml"
@@ -51,4 +59,4 @@ module "ansiblecall" {
     module.vpc,
     module.vm
   ]
-}
+} */
